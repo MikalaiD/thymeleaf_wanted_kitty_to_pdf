5fa83bdf90732134f0afc65d2ed8ff4688d590bf
DESKTOP-4928LN9\dudko <dudko.na@gmail.com>
2022-11-16 21:16:36 +0100

    toggle works

--- app/src/main/java/com/kittywanted/adapters/posterservice/FormattingService.java
+++ app/src/main/java/com/kittywanted/adapters/posterservice/FormattingService.java
@@ -3,29 +3,17 @@ package com.kittywanted.adapters.posterservice;
 import com.kittywanted.domain.model.Format;
 import java.util.Map;
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
+import lombok.RequiredArgsConstructor;
 
-@Component
-@Scope(
-    value = WebApplicationContext.SCOPE_SESSION,
-    proxyMode = ScopedProxyMode.TARGET_CLASS)
+
 @Getter
-@Setter
+@RequiredArgsConstructor
 public class FormattingService {
 
   public static final String THEME = "theme";
   public static final String LIGHT = "light";
   public static final String DARK = "dark";
-  private Map<String, String> properties;
-
-  public FormattingService(Map<String, String> properties) {
-    this.properties = properties;
-    properties.put(THEME, LIGHT);
-  }
+  private final Map<String, String> properties;
 
   public Format toDomain() {
     return new Format(properties);
--- app/src/main/java/com/kittywanted/config/WebConfig.java
+++ app/src/main/java/com/kittywanted/config/WebConfig.java
@@ -1,15 +1,32 @@
 package com.kittywanted.config;
 
+import com.kittywanted.adapters.posterservice.FormattingService;
+import java.util.HashMap;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry
         .addResourceHandler("/webjars/**")
         .addResourceLocations("/webjars/");
+  }
+
+  @Bean
+  @Scope(
+      value = WebApplicationContext.SCOPE_SESSION,
+      proxyMode = ScopedProxyMode.TARGET_CLASS)
+  public FormattingService formattingService() {
+    var defaultProperties = new HashMap<String, String>();
+    defaultProperties.put("theme", "light");
+    return new FormattingService(defaultProperties);
   }
 }
